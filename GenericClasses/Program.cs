228c6df34f94e9509fd471893803fa7349c577db
Coreplo <coreplo@gmail.com>
2019-07-09 23:55:26 +0200

    class generics

--- GenericClasses/Program.cs
+++ GenericClasses/Program.cs
@@ -6,16 +6,52 @@ using System.Threading.Tasks;
 
 namespace GenericClasses
 {
+
+    struct Person
+    {
+        string Name;
+        int Age;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Generic Classes");
+
+            Stack<int> intStack = new Stack<int>();
+            Stack<string> strStack = new Stack<string>();
+            Stack<Person> PersonStack = new Stack<Person>();
+
+
+            intStack.Push(3);
+            intStack.Push(6);
+            intStack.Push(7);
+
+            Console.WriteLine(intStack.Get(1));
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+    }
+
+    class Stack<T>
+    {
+        int index = 0;
+        T[] innerArray = new T[100];
+        public void Push(T item)
+        {
+            innerArray[index++] = item;
+        }
+        public T Pop()
+        {
+            return innerArray[--index];
+        }
+        public T Get(int k)
+        {
+            return innerArray[k];
+        }
+
     }
 }
